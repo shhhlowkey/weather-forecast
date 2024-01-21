2e2212ce6d9b0c749a4b23794624d6e56930b68e
Shhhlowkey <154291741+Shhhlowkey@users.noreply.github.com>
2024-01-21 11:26:45 +1000

    added API to search functionality

--- src/script.js
+++ src/script.js
@@ -1,10 +1,27 @@
+function refreshData(response) {
+  let tempElement = document.querySelector("#current-temp");
+  let cityElement = document.querySelector("#current-city");
+  let temperature = response.data.temperature.current;
+
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temperature);
+}
+
+function apiCitySearch(city) {
+  let apiKey = "6a97550f4fc033e330d0ot4a7bb681ab";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshData);
+}
+
 function handleCitySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-field");
-  let cityElement = document.querySelector("#current-city");
 
-  cityElement.innerHTML = searchInput.value;
+  apiCitySearch(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleCitySearch);
+
+apiCitySearch("Brisbane");
